5d34027c242213a9ecfcfadd06f35aaa9cac14f5
Zhongwei Liu <whitney.liu@mail.utoronto.ca>
2023-08-27 22:27:50 -0700

    add login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
 import LandingPage from "./components/pages/landing-page/LandingPage";
+import LoginPage from "./components/pages/login-page/LoginPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-          <Routes>
-            <Route index element={<LandingPage />} />
-            {/* <Route path="login" element={<LoginPage />} />
-            <Route path="signup" element={<SignupPage />} />
+        <Routes>
+          <Route index element={<LandingPage />} />
+          <Route path="login" element={<LoginPage />} />
+          {/* <Route path="signup" element={<SignupPage />} />
             <Route path="password-recover" element={<PasswordRecoverPage />} />
             <Route path="home" element={<Home />}>
               <Route />
               <Route />
             </Route>
             <Route path="test" element={<Test />} /> */}
-          </Routes>
-        </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
