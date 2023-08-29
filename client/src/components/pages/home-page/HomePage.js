1d9b22f1d75bdc4fa6bcf85e4e6002cbb7291a54
wxyn <100791680+XiaoyananWang@users.noreply.github.com>
2023-08-29 15:56:01 -0400

    modified greeting

--- client/src/components/pages/home-page/HomePage.js
+++ client/src/components/pages/home-page/HomePage.js
@@ -6,6 +6,7 @@ import HomepageHeader from "./components/HomepageHeader";
 import Greeting from "./components/Greeting";
 import { useSelector } from "react-redux";
 import Sidebar from "./components/Sidebar";
+import Container from "../../sharable-components/Container";
 
 export default function Home() {
   const dispatch = useDispatch();
@@ -20,9 +21,14 @@ export default function Home() {
   return (
     <>
       <HomepageHeader onLogout={handleLogout} />
-      <Sidebar />
-      <Greeting userName={userName} />
-
+      <Container className="flex">
+        <div className="w-1/4">
+          <Sidebar />
+        </div>
+        <div className="w-3/4">
+          <Greeting userName={userName} />
+        </div>
+      </Container>
     </>
   );
 }
--- client/src/components/pages/home-page/components/Greeting.js
+++ client/src/components/pages/home-page/components/Greeting.js
@@ -2,8 +2,8 @@ import React from "react";
 import Container from "../../../sharable-components/Container";
 const Greeting = ({ userName }) => {
   return (
-    <div>
-      <h2 className="text-slate-900 text-2xl font-bold">Hello, {userName}!</h2>
+    <div className="flex justify-start items-start">
+      <h2 className="text-slate-900 text-2xl font-bold ml-10">Hello, {userName}!</h2>
     </div>
   );
 };
