2efe791d4eaa2f66d582fd6a4d4d175777c400fe
wxyn <100791680+XiaoyananWang@users.noreply.github.com>
2023-09-01 16:05:08 -0400

    Modified HomePage
    Changed the design of layout, added the stats boxes UI

--- client/src/components/sharable-components/Layout.js
+++ client/src/components/sharable-components/Layout.js
@@ -5,6 +5,7 @@ import HomepageHeader from "../pages/home-page/components/HomepageHeader";
 import { logout } from "../../redux/reducer/authenticationReducer";
 import React from "react";
 import Sidebar from "../pages/home-page/components/Sidebar";
+import StatBox from "../pages/home-page/components/StatsBox";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -22,14 +23,22 @@ export default function Layout({ children }) {
 
   return (
     <>
-      <BackgroundImage className="someClassName" position="right" />
-      <HomepageHeader/>
+      <BackgroundImage
+        className="someClassName hidden md:block"
+        position="right"
+      />
+      <HomepageHeader />
       <Container className="flex relative">
-        <div className="w-1/4">
+        <div
+          className="hidden md:block w-1/4 fixed top-0 left-0"
+          style={{ height: "100vh", zIndex: 10 }}
+        >
           <Sidebar onLogout={handleLogout} />
         </div>
-        <div className="w-3/4 p-4 relative z-10">
-          <Greeting userName={userName} />
+        <div className="w-full md:w-3/4 p-4 relative z-10 ml-0 md:ml-1/4">
+          <div className="fixed left-1/4 top-0 mt-5 ml-[-100px]">
+            <Greeting userName={userName} />
+          </div>
           <div className="mt-4">{children}</div>
         </div>
       </Container>
