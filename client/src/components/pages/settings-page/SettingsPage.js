29d8e1e569e78fd50bd532071247eadf85c69016
Abi <abiweiyang0913@gmail.com>
2023-09-02 00:51:25 -0400

    non-interactive settingpage

--- client/src/components/pages/settings-page/SettingsPage.js
+++ client/src/components/pages/settings-page/SettingsPage.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Layout from "../../sharable-components/Layout";
+import ClearSignedFiles from "./components/ClearSignedFiles";
+import DeleteAccount from "./components/DeleteAccount";
+
 export default function SettingsPage() {
   return (
     <Layout>
-      {" "}
-      <div>This is the SettingsPage!</div>
+      <div className=" fixed left-1/4 top-0 mt-16 ml-[-60px] w-full md:w-3/4">
+        <div className=" w-7/12 mt-20">
+          <ClearSignedFiles />
+          <DeleteAccount />
+        </div>
+      </div>
     </Layout>
   );
 }
