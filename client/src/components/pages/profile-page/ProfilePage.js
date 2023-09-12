b60d018f7a3e65ef34a02d9caceac7bf1afba67a
wxyn <100791680+XiaoyananWang@users.noreply.github.com>
2023-09-12 16:25:39 -0400

    Updated ProfilePage
    ProfilePage overlap problem still need to be fixed, logout problem still need to be fixed.

--- client/src/components/pages/profile-page/ProfilePage.js
+++ client/src/components/pages/profile-page/ProfilePage.js
@@ -3,14 +3,15 @@ import ProfileGreeting from "./components/ProfileGreeting";
 import PersonalInfoForm from "./components/PersonalInfoForm";
 import React from "react";
 
-
-
 export default function ProfilePage() {
-
   return (
     <Layout>
-      <ProfileGreeting />
-      <PersonalInfoForm />
+      <div className="relative md:sticky left-1/4 top-0 mt-5 ml-[-125px] w-full md:w-1/4 z-10">
+        <ProfileGreeting />
+      </div>
+      <div className="relative md:sticky left-1/4 top-0 mt-[90px] ml-[-125px] w-full md:w-3/4 z-10">
+        <PersonalInfoForm />
+      </div>
     </Layout>
   );
 }
