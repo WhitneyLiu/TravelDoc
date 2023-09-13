8765a25bf59a1eb5434a89c918c19d4b8123609a
wxyn <100791680+XiaoyananWang@users.noreply.github.com>
2023-09-13 13:37:57 -0400

    Bug fixed
    Fixed bug of profile form overlap with sidebar on mac screen

--- client/src/components/pages/profile-page/ProfilePage.js
+++ client/src/components/pages/profile-page/ProfilePage.js
@@ -6,10 +6,11 @@ import React from "react";
 export default function ProfilePage() {
   return (
     <Layout>
-      <div className="relative md:sticky left-1/4 top-0 mt-5 ml-[-125px] w-full md:w-1/4 z-10">
+      <div className="relative md:fixed left-1/4 top-0 mt-5 ml-[-125px] w-full md:w-1/4 z-10">
         <ProfileGreeting />
       </div>
-      <div className="relative md:sticky left-1/4 top-0 mt-[90px] ml-[-125px] w-full md:w-3/4 z-10">
+
+      <div className="relative md:fixed left-1/4 top-0 mt-[120px] ml-[-125px] w-full md:w-3/4 z-10 max-h-[calc(100vh-200px)] overflow-y-auto pr-4">
         <PersonalInfoForm />
       </div>
     </Layout>
