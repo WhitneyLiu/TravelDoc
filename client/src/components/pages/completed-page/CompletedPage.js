0bb9563ec00c3a3221aa1a93ff07dac38e867ba6
Abi <140275439+GrAgonBi@users.noreply.github.com>
2023-09-16 01:42:52 -0400

    CompletedPage Update

--- client/src/components/pages/completed-page/CompletedPage.js
+++ client/src/components/pages/completed-page/CompletedPage.js
@@ -2,11 +2,10 @@ import React, { useState } from "react";
 import PdfContainer from "./components/PdfContainer";
 import PdfViewer from "./components/PdfViewer";
 import Layout from "../../sharable-components/Layout";
-
 export default function CompletedPage() {
   const [selectedPdf, setSelectedPdf] = useState(null);
 
-  const pdfFiles = [
+  const mockPdfFiles = [
     { name: "Doc1", url: "#", timestamp: "2023/1/12" },
     { name: "Doc2", url: "#", timestamp: "2023/3/15" },
     { name: "Doc3", url: "#", timestamp: "2023/5/20" },
@@ -24,11 +23,12 @@ export default function CompletedPage() {
 
   return (
     <Layout>
-      <div className="fixed left-1/4 top-0 mt-16 ml-[-60px] w-full md:w-3/4">
+
+      <div className="relative md:fixed left-1/4 top-0 mt-16 ml-[-60px] w-full md:w-3/4 z-10  max-h-[90vh] overflow-y-auto pr-4">
         {selectedPdf ? (
           <PdfViewer pdfUrl={selectedPdf} />
         ) : (
-          <PdfContainer pdfFiles={pdfFiles} onPdfClick={handlePdfClick} />
+          <PdfContainer pdfFiles={mockPdfFiles} onPdfClick={handlePdfClick} />
         )}
       </div>
     </Layout>
--- client/src/components/pages/completed-page/components/PdfContainer.js
+++ client/src/components/pages/completed-page/components/PdfContainer.js
@@ -1,38 +1,67 @@
-import PdfLogo from "./PdfLogo";
+
 import Button from "../../../sharable-components/Button";
 import { useState } from "react";
+import PdfBoxPagination from "../../home-page/components/PdfBoxPagination";
+import { DocumentIcon } from "@heroicons/react/24/outline";
 
 export default function PdfContainer({ pdfFiles, onPdfClick }) {
+  const [isSorted, setIsSorted] = useState(false);
   const [completedFiles, setCompletedFiles] = useState(pdfFiles);
-  function sortByDate() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 1;
+  const totalPages = Math.ceil(completedFiles.length / itemsPerPage);
+  const offset = (currentPage - 1) * itemsPerPage;
+  const currentItems = completedFiles.slice(offset, offset + itemsPerPage);
+  
+  
+  function SortByDate() {
     const sortedFiles = [...pdfFiles].sort(
       (a, b) => new Date(b.timestamp) - new Date(a.timestamp)
     );
     setCompletedFiles(sortedFiles);
+    setIsSorted((s)=>(!s));
+  }
+
+  function SortByDefault() {
+    setCompletedFiles(pdfFiles);
+    setIsSorted((s)=>(!s));
   }
   return (
     <div>
       <div className=" flex justify-start items-start">
-        <h4 className="text-slate-900 text-2xl font-bold pl-0">
+        <h4 className="text-slate-900 text-l  pl-0">
           Here's all the docs you have signed
         </h4>
       </div>
       <div>
-        <div className="flex justify-end">
-          <Button onClick={sortByDate}>SortByDate</Button>
+        <div className="flex justify-end ">
+          <Button onClick={!isSorted?SortByDate:SortByDefault} className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            {!isSorted?"SortByDate":"SortByDefault"}
+          </Button>
         </div>
-        <div className=" mt-5 grid grid-cols-1 gap-14 sm:grid-cols-6">
-          {completedFiles.map((pdf, index) => (
+        <div className=" mt-5 flex flex-wrap justify-start items-start gap-6 w-full h-[800px] rounded-lg border-2 border-dashed border-gray-400 p-12 text-center">
+          {currentItems.map((pdf, index) => (
             <div key={index} onClick={() => onPdfClick(pdf.url)}>
-              <div className="cursor-pointer w-20">
-                <PdfLogo />
-                <p className="text-slate-900 font-bold">{pdf.name}</p>
-                <p className="text-slate-900 font-bold">{pdf.timestamp}</p>
+              <div className="flex flex-col items-center cursor-pointer ">
+                  <DocumentIcon
+                  className="h-12 w-12 text-gray-400"
+                  aria-hidden="true"
+                  />
+                <span className="mt-2 text-sm font-semibold text-gray-900">{pdf.name}</span>
+                <span className="text-sm font-semibold text-gray-900">{pdf.timestamp}</span>
               </div>
             </div>
           ))}
         </div>
       </div>
+      <PdfBoxPagination 
+        currentPage={currentPage} 
+        setCurrentPage={setCurrentPage} 
+        totalPages={totalPages} 
+        offset={offset} 
+        currentItems={currentItems} 
+        pdfList={completedFiles} 
+      />
     </div>
   );
 }
